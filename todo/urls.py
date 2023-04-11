eda96b7655607f05a62785de9a7da38d8b5cac29
Xusrav <xusrav200329@gmail.com>
2023-04-11 10:52:19 +0500

    Add login and logout forms in views.py file, and restricted users who don	 login to my web-site

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='detail'),
     path('task/create/', TaskCreate.as_view(), name='create'),
--- todo/views.py
+++ todo/views.py
@@ -1,38 +1,48 @@
 from django.views.generic import ListView, DetailView
 from .models import Task
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
-
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class TaskList(ListView):
+
+class CustomLogin(LoginView):
+    template_name = 'todo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('list')
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'todo/homepage.html'
     context_object_name = 'tasks'
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'todo/detail.html'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     template_name = 'todo/create.html'
     fields = '__all__'
     success_url = reverse_lazy('list')
 
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     template_name = 'todo/create.html'
     success_url = reverse_lazy('list')
 
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = 'todo/delete.html'
     success_url = reverse_lazy('list')
