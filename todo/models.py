8ff7cd78cfa9dfa51f1fa395614ff761b3c78176
Xusrav <xusrav200329@gmail.com>
2023-04-11 09:30:19 +0500

    Create model in models.py, make migrate and connect this model with admin panel, than create a superuser and runserver for cheking our models in admin panel

---
+++ todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField("Загаловок", max_length=70)
+    description = models.CharField("Описание", max_length=500)
+    complete = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['complete']
